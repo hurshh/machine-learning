b1295b4737b2056fa05de9564475cfef05577066
hurshh <quasarhrsh@gmail.com>
2021-11-19 17:00:40 +0530

    MultipleLinearRegression.py

--- MultipleLinearRegression.py
+++ MultipleLinearRegression.py
@@ -10,10 +10,25 @@ y = dataset.iloc[:, -1].values
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 
-ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])],remainder='passthrough')
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])], remainder='passthrough')
 x = ct.fit_transform(x)
 
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2,random_state=0)
 
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+
+from sklearn.linear_model import LinearRegression
+
+regressor = LinearRegression()
+regressor.fit(x_train, y_train)
+
+# print(x_train)
+# print()
+# print(y_train)
+
+# predicting result
+
+y_pred = regressor.predict(x_test)
+np.set_printoptions(precision=2)
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), (y_test.reshape(len(y_pred), 1))), 1))
 
