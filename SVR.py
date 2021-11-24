3c77a262438905cce3bdba8d128821e93174e4e6
hurshh <quasarhrsh@gmail.com>
2021-11-24 17:08:48 +0530

    svr feature scalling done

---
+++ SVR.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('dataSets/Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/Python/Position_Salaries.csv')
+
+x = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:, -1].values
+
+y = y.reshape(len(y),1)
+
+# feature scaling
+
+from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+sc_y = StandardScaler()
+x = sc_x.fit_transform(x)
+y = sc_y.fit_transform(y)
+
+# print(x)
+# print(y)
